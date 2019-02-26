1241ace17f5fc48e74cd47da0b5d39456b4e5fac
Elliot Yoon <elliotyoon@outlook.com>
2019-02-26 14:22:24 -0800

    Fixed WeeklyServlet for new schema.

--- src/main/WeeklyServlet.java
+++ src/main/WeeklyServlet.java
@@ -26,32 +26,18 @@ public class WeeklyServlet extends HttpServlet {
         String sectionID = request.getParameter("sectionID");
 
         dbConn.openConnection();
-        PreparedStatement stmtWeekly = dbConn.getPreparedStatment("INSERT INTO weekly_meetings VALUES(?::day_enum, ?::TIME, ?::TIME, ?, ?, ?::meeting_enum, ?)");
-        PreparedStatement stmtHas = dbConn.getPreparedStatment("INSERT INTO has_weekly_meetings VALUES(?, ?::day_enum, ?::TIME, ?::TIME, ?, ?)");
+        PreparedStatement stmtHas = dbConn.getPreparedStatment("INSERT INTO has_weekly_meetings VALUES(?, ?::day_enum, ?::TIME, ?::TIME, ?, ?, ?, ?)");
         try {
-            stmtWeekly.setString(1, day);
-            stmtWeekly.setString(2, timeStart);
-            stmtWeekly.setString(3, timeEnd);
-            stmtWeekly.setString(4, building);
-            stmtWeekly.setString(5, room);
-            stmtWeekly.setString(6, typeMeeting);
-            stmtWeekly.setBoolean(7, required);
-
             stmtHas.setString(1, sectionID);
             stmtHas.setString(2, day);
             stmtHas.setString(3, timeStart);
             stmtHas.setString(4, timeEnd);
             stmtHas.setString(5, building);
             stmtHas.setString(6, room);
+            stmtHas.setString(7, typeMeeting);
+            stmtHas.setBoolean(8, required);
         } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-
-        boolean resultWeekly = dbConn.executePreparedStatement(stmtWeekly);
-        if (!resultWeekly) {
-            System.out.println("weekly statement failed!");
-            dbConn.closeConnections();
-            return;
         }
 
         boolean resultHas = dbConn.executePreparedStatement(stmtHas);
